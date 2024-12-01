3d9ca8bcd26d49772eaf86620c1bc446b8f15c66
seb <swalak@gmail.com>
2024-12-01 17:16:22 +0000

    Increased reuse in WireMockTestUtil.
    Added parameter that controls logging discovery/creation of auto-generated WireMock stubs.

--- src/test/java/com/sebwalak/seln/spring_exercise/WireMockTestUtil.java
+++ src/test/java/com/sebwalak/seln/spring_exercise/WireMockTestUtil.java
@@ -5,6 +5,7 @@ import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.common.ConsoleNotifier;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.matching.StringValuePattern;
+import lombok.extern.log4j.Log4j2;
 
 import java.io.File;
 import java.nio.file.Path;
@@ -22,50 +23,16 @@ import static org.springframework.http.HttpHeaders.ACCEPT;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+interface ProxyStubMaker {
+    void createProxyStub(String pathElementName, String queryParameterName, StringValuePattern queryPattern, Path stubResponseBody);
+}
+
+@Log4j2
 public class WireMockTestUtil {
 
     public static final String VALID_API_KEY = "validApiKey";
     public final static String MOCKED_LIFELIKE_COMPANY_NUMBER = "06500244";
     public final static String MOCKED_LIFELIKE_COMPANY_NAME = "BBC LIMITED";
-    public final static String MOCKED_COMPANY_NUMBER_NO_OFFICERS = "01481686";
-    public final static String MOCKED_COMPANY_NAME = "BBC LIMITED";
-
-    private static void stubCompanySearch(
-            StringValuePattern queryPattern,
-            String filenameWithResponseBody,
-            String wireMockContextPath,
-            WireMockServer wireMockServer) {
-
-        System.out.println("query=" + queryPattern.getName() + " " + queryPattern.getValue() + " filename=" + filenameWithResponseBody);
-        wireMockServer.stubFor(
-                get(urlPathEqualTo(format("%s/v1/Search", wireMockContextPath)))
-                        .withQueryParam("Query", queryPattern)
-                        .withHeader(ACCEPT, equalTo(APPLICATION_JSON_VALUE))
-                        .withHeader(HEADER_API_KEY, equalTo(VALID_API_KEY))
-                        .willReturn(aResponse()
-                                .withStatus(200)
-                                .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
-                                .withBodyFile(filenameWithResponseBody)));
-    }
-
-    private static void stubOfficerSearch(
-            StringValuePattern companyNumberPattern,
-            String filenameWithResponseBody,
-            String wireMockContextPath,
-            WireMockServer wireMockServer) {
-
-        System.out.println("query=" + companyNumberPattern.getName() + " " + companyNumberPattern.getValue() + " filename=" + filenameWithResponseBody);
-
-        wireMockServer.stubFor(
-                get(urlPathEqualTo(format("%s/v1/Officers", wireMockContextPath)))
-                        .withQueryParam("CompanyNumber", companyNumberPattern)
-                        .withHeader(ACCEPT, equalTo(APPLICATION_JSON_VALUE))
-                        .withHeader(HEADER_API_KEY, equalTo(VALID_API_KEY))
-                        .willReturn(aResponse()
-                                .withStatus(200)
-                                .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
-                                .withBodyFile(filenameWithResponseBody)));
-    }
 
     public static WireMockServer setUp(String wireMockContextPath, boolean verbose) {
         WireMockConfiguration wireMockConfiguration = wireMockConfig().dynamicPort();
@@ -74,58 +41,69 @@ public class WireMockTestUtil {
         }
         WireMockServer wireMockServer = new WireMockServer(wireMockConfiguration);
 
+        ProxyStubMaker proxyStubMaker = (pathElementName, queryParameterName, queryPattern, stubResponseBody) ->
+                wireMockServer.stubFor(
+                        get(urlPathEqualTo(format("%s/v1/%s", wireMockContextPath, pathElementName)))
+                                .withHeader(ACCEPT, equalTo(APPLICATION_JSON_VALUE))
+                                .withHeader(HEADER_API_KEY, equalTo(VALID_API_KEY))
+                                .withQueryParam(queryParameterName, queryPattern)
+                                .willReturn(aResponse()
+                                        .withStatus(200)
+                                        .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                                        .withBodyFile(stubResponseBody.toString())
+                                )
+                );
+
         // scanning `basePath` location for files and for each found file using
         // directory and file name structure to create appropriate stubs
         Path basePath = Paths.get("src/test/resources/__files");
 
-        Consumer<Path> createProxyCompanySearchStub = path -> {
-            String fileNameNoExt = path.getFileName().toString();
-            fileNameNoExt = fileNameNoExt.substring(0, fileNameNoExt.lastIndexOf('.'));
+        Consumer<Path> pathToStub = path -> {
+            String fileNameNoExt = getFilenameWithoutExtension(path.getFileName().toString());
+            StringValuePattern queryParameterValueMatcher = equalTo(fileNameNoExt);
+            Path pathWithResponseBody = basePath.relativize(path);
 
-            stubCompanySearch(
-                    equalTo(fileNameNoExt),
-                    basePath.relativize(path).toString(),
-                    wireMockContextPath,
-                    wireMockServer);
+            log.debug("auto-stubbing query={}({}) => filename={}",
+                    queryParameterValueMatcher.getName(), queryParameterValueMatcher.getValue(), pathWithResponseBody);
 
+            if (path.toString().matches("^.*/proxy/search/.*[.]json$")) {
+                proxyStubMaker.createProxyStub(
+                        "Search",
+                        "Query",
+                        queryParameterValueMatcher,
+                        pathWithResponseBody);
+            } else if (path.toString().matches("^.*/proxy/officers/.*[.]json$")) {
+                proxyStubMaker.createProxyStub(
+                        "Officers",
+                        "CompanyNumber",
+                        queryParameterValueMatcher,
+                        pathWithResponseBody);
+            }
         };
 
-        Consumer<Path> createProxyOfficersSearchStub = path -> {
-            String fileNameNoExt = path.getFileName().toString();
-            fileNameNoExt = fileNameNoExt.substring(0, fileNameNoExt.lastIndexOf('.'));
-
-            stubOfficerSearch(
-                    equalTo(fileNameNoExt),
-                    basePath.relativize(path).toString(),
-                    wireMockContextPath,
-                    wireMockServer);
-        };
-
-        Stream.of("src/test/resources/__files/life-like/proxy/search",
-                        "src/test/resources/__files/minimal/proxy/search")
-                .map(File::new)
-                .map(File::listFiles)
+        Stream.of("life-like/proxy/search",
+                        "minimal/proxy/search",
+                        "life-like/proxy/officers",
+                        "minimal/proxy/officers")
+                .map(basePath::resolve)                     // "prepend" the string with basePath
+                .map(Path::toFile)
+                .map(File::listFiles)                       // directory listing
                 .filter(Objects::nonNull)
-                .flatMap(Arrays::stream)
+                .flatMap(Arrays::stream)                    // will flatten nested collections
                 .filter(File::isFile)
                 .map(File::toPath)
-                .forEach(createProxyCompanySearchStub);
-
-        Stream.of("src/test/resources/__files/life-like/proxy/officers",
-                        "src/test/resources/__files/minimal/proxy/officers")
-                .map(File::new)
-                .map(File::listFiles)
-                .filter(Objects::nonNull)
-                .flatMap(Arrays::stream)
-                .filter(File::isFile)
-                .map(File::toPath)
-                .forEach(createProxyOfficersSearchStub);
+                .forEach(pathToStub);                       // analyse path and then crete WireMock stub
 
         wireMockServer.start();
 
         WireMock.configureFor(wireMockServer.port());
 
         return wireMockServer;
+    }
+
+    private static String getFilenameWithoutExtension(String fileNameNoExt) {
+        fileNameNoExt = fileNameNoExt.substring(0, fileNameNoExt.lastIndexOf('.'));
+        return fileNameNoExt;
     }
 
     public static void tearDown(WireMockServer wireMockServer) {
